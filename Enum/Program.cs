b915111208fa56286fe3790cf8e04f265835271e
nijatmammadovv <tu7q78xlv@code.edu.az>
2022-03-27 13:30:32 +0400

    Add project files.

---
+++ Enum/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using Enum;
+
+namespace Enum
+{
+    class Test
+    {
+        static void Main(string[] args)
+        {
+            //Enum
+            int day = 6;
+            switch (day)
+            {
+                case (int) @enum.Days.Monday:
+                    Console.WriteLine("---------------");
+                    Console.WriteLine("Wake up school time");
+                    Console.WriteLine("Monday");
+                    Console.WriteLine("---------------");
+                    break;
+                case (int)@enum.Days.Tuesday:
+                    Console.WriteLine("---------------");
+                    Console.WriteLine("You have to go to work today");
+                    Console.WriteLine("Tuesday");
+                    Console.WriteLine("---------------");
+                    break;
+                case (int)@enum.Days.Wednesday:
+                    Console.WriteLine("---------------");
+                    Console.WriteLine("Wednesday");
+                    Console.WriteLine("---------------");
+                    break;
+                case (int)@enum.Days.Thursday:
+                    Console.WriteLine("---------------");
+                    Console.WriteLine("Thursday");
+                    Console.WriteLine("---------------");
+                    break;
+                case (int)@enum.Days.Friday:
+                    Console.WriteLine("---------------");
+                    Console.WriteLine("Friday");
+                    Console.WriteLine("---------------");
+                    break;
+                case (int)@enum.Days.Saturday:
+                    Console.WriteLine("---------------");
+                    Console.WriteLine("Saturday");
+                    Console.WriteLine("---------------");
+                    break;
+                case (int)@enum.Days.Sunday:
+                    Console.WriteLine("---------------");
+                    Console.WriteLine("It's a day off");
+                    Console.WriteLine("Sunday");
+                    Console.WriteLine("---------------");
+                    break;
+                default:
+                    Console.WriteLine("---------------");
+                    Console.WriteLine("Belə bir gün hələ kəşf olunmayıb!");
+                    Console.WriteLine("---------------");
+                    break;
+            }
+
+        }
+    }
+}
---
+++ Enum/enum.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Enum
+{
+    class @enum 
+    {
+      public enum Days { Monday, Tuesday,Wednesday,Thursday,Friday,Saturday,Sunday };
+    }
+}
